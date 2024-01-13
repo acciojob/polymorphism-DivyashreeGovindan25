564c736fb381db722b0a3bf932ed1382eedab4b8
HP <divyashreeharitha@gmail.com>
2024-01-13 18:49:16 +0530

    First commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
-
+import java.util.*;
 public class Main {
-
+    public static void main(String[] args){
+        Product p = new Product();
+        System.out.println(p.product(5,10));
+        System.out.println(p.product(6,1,4));
+        System.out.println(p.product(2.6,8.8));
+    }
+}
+class Product{
+    public int product(int x, int y) { return x+y; }
+    public int product(int x, int y, int z) { return x+y+z; }
+    public double product(double x, double y) { return x+y; }
 }
