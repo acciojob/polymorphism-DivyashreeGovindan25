8a029afdf9eb56059bb8f0e7e1d24689eed55c98
HP <divyashreeharitha@gmail.com>
2024-01-15 11:55:22 +0530

    version2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,10 +2,18 @@ package com.driver;
 import java.util.*;
 public class Main {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         Product p = new Product();
-        p.product(5,10);
-        p.product(6,1,4);
-        p.product(2.6,8.8);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(p.product(a,b));
+        int a1 = sc.nextInt();
+        int b1 = sc.nextInt();
+        int c1 = sc.nextInt();
+        System.out.println(p.product(a1,b1,c1));
+        double a2 = sc.nextDouble();
+        double b2 = sc.nextDouble();
+        System.out.println(p.product(a2,b2));
     }
 }
 class Product{
